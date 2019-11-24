eea9b04eead443b37cc3334160f9b3befec2b075
Jodi N Kansagor <joedvice@gmail.com>
2019-11-24 09:54:39 -0800

    giving me an error. files.upload is undefined because files is an empty object

---
+++ requestHandlers.js
@@ -0,0 +1,57 @@
+const querystring = require('querystring');
+const fs = require('fs');
+const formidable = require('formidable');
+
+function start(res) {
+    console.log('Request handler "start" was called.');
+
+    const body = '<html>' +
+        '<head>' +
+        '<meta http-equiv="Content-Type" content="text/html; ' +
+        'charset=UTF-8" />' +
+        '</head>' +
+        '<body>' +
+        '<form action="/upload" enctype="multipart/form-data>" ' + 'method="post">' +
+        '<input type="file" name="upload">' +
+        '<input type="submit" value="Upload file" />' +
+        '</form>' +
+        '</body>' +
+        '</html>';
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(body);
+    res.end();
+}
+
+function upload(res, req) {
+    console.log('Request handler "upload" was called.');
+
+    const form = new formidable.IncomingForm();
+    console.log('about to parse');
+    form.parse(req, function(error, fields, files){
+        console.log('parsing done');
+        console.log(files, 'FILES');
+        console.log(files.upload, 'FILES.UPLOAD');
+        console.log(files.upload.path, 'FILES.UPLOAD.PATH');
+        fs.rename(files.upload.path, '/tmp/test.png', function(error) {
+            if (error) {
+                fs.unlink('tmp/test.png');
+                fs.rename(files.upload.path, '/tmp/test.png');
+            }
+        });
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('received image: <br>');
+        res.write('<img src="/show" />');
+        res.end();
+    });
+}
+
+function show(res) {
+    console.log('Request handler "show" was called.');
+    res.writeHead(200, { 'Content-Type': 'image/png' });
+    fs.createReadStream('/tmp/test.png').pipe(res);
+}
+
+exports.start = start;
+exports.upload = upload;
+exports.show = show;
